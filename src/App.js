7b8a7f1c57f0e1d84dbb361e01e7139cd30888f1
rafael-wiegand <rafaelwiegand@hotmail.com>
2025-07-15 22:31:20 -0300

    atualizando  o projeto4

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom'; // Importando o hook useLocation
+//import React, { useEffect } from 'react';
+//import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom'; // Importando o hook useLocation
 import './App.css';
 import HeaderComponent from './components/Header/header';
 import SobreComponent from './components/Section/sobre';
